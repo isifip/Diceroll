52a5c950e80235e5463dd79be8abbf20c9f11340
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-21 14:47:04 +0400

    first commit

---
+++ Diceroll/DicerollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicerollApp.swift
+//  Diceroll
+//
+//  Created by Irakli Sokhaneishvili on 21.01.22.
+//
+
+import SwiftUI
+
+@main
+struct DicerollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
